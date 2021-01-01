aae9e0db7556a866845cdc26307ae3b50a75d4f1
马少博 <mashaobo@inspur.com>
2021-01-01 10:11:09 +0800

    测试jenkins

--- router/custom.js
+++ router/custom.js
@@ -109,7 +109,7 @@ async function handleMusicList(musicListRes, req) {
             const x = beforeMusicListArr[index];
             let musicDetailObj = musicsDetail.get(x.id);
             let musicObj = {
-                'by':'OldsixLi',
+                'by': 'OldsixLi6666666',
                 // name: x.name,
                 // author: x && x.ar && x.ar[0] && x.ar[0].name || "",
                 id: x.id,
